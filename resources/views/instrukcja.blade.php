3544fe1cdefa9aab53b5ac9b5980633b26ca2714
dariusia <30595371+migdalius@users.noreply.github.com>
2018-04-09 15:49:30 +0200

    Zarządzanie kontami
    Wstęne przygotowanie wyglądku tej zakładki.

--- resources/views/instrukcja.blade.php
+++ resources/views/instrukcja.blade.php
@@ -8,7 +8,7 @@
                         </div>
                     @endif
 
-              <div class="col-md-8 order-md-1">
+              <div class="col-md-12 order-md-1">
           			<h4 class="mb-3">1. Logujemy się na konto allegro z którego chcemy pobrać nasz klucz webapi, w moim przypadku jest to konto allegro: Falxmen</h4>
           			<div class="row">
             	<div class="col-md-4 mb-3">
@@ -17,7 +17,7 @@
             </div>
           	  </div>
           	  <br>
-          	  <div class="col-md-8 order-md-1">
+          	  <div class="col-md-12 order-md-1">
           			<h4 class="mb-3">2. Następnie wchodzimy w "Moje konto" i na dole strony znajduje się webapi link (informacje i ustawienia) klikamy w niego</h4>
           			<div class="row">
             	<div class="col-md-4 mb-3">
@@ -26,7 +26,7 @@
             </div>
           	  </div>		
           	  <br>
-          	  <div class="col-md-8 order-md-1">
+          	  <div class="col-md-12 order-md-1">
           			<h4 class="mb-3">3. Jeżeli klucz został juz wygenerowany kopiujemy go jeżeli nie tworzymy nowy, zaznaczamy opcje dla użytkownika programu, kolumne do jakich celów zostanie użyte api zostawiamy pustą lub wpisujemy do wystawiania aukcji.</h4>
           			<div class="row">
             	<div class="col-md-4 mb-3">
--- resources/views/wystaw.blade.php
+++ resources/views/wystaw.blade.php
@@ -8,6 +8,75 @@
                         </div>
                     @endif
 
-                   tutaj będzie wystawianie aukcjie
+               <div class="col-md-12 order-md-1">
+          			<h4 class="mb-3">1. Przygotuj się do wystania nowego dostawcy</h4>
+          			<div class="row">
+             <div class="form-group">
+             	 <div class="col-md-4 order-md-1">
+    			<label for="exampleFormControlSelect1"></label>
+    				<select class="form-control" id="exampleFormControlSelect1">
+      				<option>Kesi</option>
+      				<option>Sporti</option>
+      				<option>Czasnabuty</option>
+      				<option>Kupbuty</option>
+    			</select>
+    		</div>
+  			</div>
+          </div>
+          <div class="container col-md-8">
+  <h2></h2>
+  <p></p>            
+  <table class="table table-bordered ">
+    <thead>
+      <tr>
+        <th>Kategoria 1</th>
+        <th>Kategoria 2</th>
+        <th>Tagi</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>różówe</td>
+        <td>łądne</td>
+        <td>ładniejsze</td>
+      </tr>
+      <tr>
+        <td>Czarne</td>
+        <td>Złote</td>
+        <td>piękne</td>
+      </tr>
+      <tr>
+        <td>takie</td>
+        <td>dakie</td>
+        <td>super łądne</td>
+      </tr>
+    </tbody>
+  </table>
+   <hr class="mb-4">
+</div>
+</div>
+
+<div class="col-md-12 order-md-1">
+   <h4 class="mb-3">2. Zarządzaj ceną:</h4>
+   <div class="row">
+              <div class="col-md-2 mb-3">
+                <label for="marza">Marża (%)</label>
+                <input type="text" class="form-control" id="marza" placeholder="" value="" required>
+              </div>
+
+              <div class="col-md-2 mb-3">
+                <label for="dodatek">Dodatek (np: 0,10)</label>
+                <input type="text" class="form-control" id="dodatek" placeholder="" value="" required>
+              </div>
+            </div>
+             <hr class="mb-4">
+</div>
+
+<div class="col-md-12 order-md-1">
+          			<h4 class="mb-3">3.Wystaw aukcje allegro</h4>
+          		</div>
+
+
+
                
 @endsection
