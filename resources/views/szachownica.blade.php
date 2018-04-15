b857464603b3688840e6dce5657f8032998cddd4
dariusia <30595371+migdalius@users.noreply.github.com>
2018-04-15 16:42:50 +0200

    szachownica 0.1v
    start szachownicy : działa wyszukiwanie pagnacja i editinline

--- resources/views/szachownica.blade.php
+++ resources/views/szachownica.blade.php
@@ -2,14 +2,14 @@
 <html lang="pl">
 <head>
 
-    <title>Bootstrap - Data Table</title>
+    <title>Szachownica - Beta v0.1</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="{{asset ('tabela2/css/vendor/bootstrap.min.css')}}" type="text/css" rel="stylesheet">
     <link href="{{asset ('tabela2/css/vendor/font-awesome.min.css')}}" type="text/css" rel="stylesheet">
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600' rel='stylesheet' type='text/css'>
     <link href="{{asset ('tabela2/css/jquery.bdt.css')}}" type="text/css" rel="stylesheet">
     <link href="{{asset ('tabela2/css/style.css')}}" type="text/css" rel="stylesheet">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="http://code.jquery.com/jquery-2.1.1.min.js" type="text/javascript"></script>
     <link href="https://www.jqueryscript.net/css/jquerysctipttop.css" rel="stylesheet" type="text/css">
 
 
@@ -18,8 +18,8 @@
 <div class="container">
     <div class="row">
             <div class="box clearfix">
-            <h3>Bootstrap Data Table</h3>
-            <p>Easily turn your tables into datatables.</p>
+            <h3>Szachownica</h3>
+            <p>Beta v0.1</p>
 
             <table class="table table-hover" id="bootstrap-table">
                 <thead>
@@ -254,7 +254,6 @@
     </div>
 </div>
 <script src="{{ asset('tabela/jquery.tabledit.js') }}"></script>
-<script src="http://code.jquery.com/jquery-2.1.1.min.js" type="text/javascript"></script>
 <script src="{{asset ('tabela2/js/vendor/bootstrap.min.js')}}" type="text/javascript"></script>
 <script src="{{asset ('tabela2/js/vendor/jquery.sortelements.js')}}" type="text/javascript"></script>
 <script src="{{asset ('tabela2/js/jquery.bdt.min.js')}}" type="text/javascript"></script>
